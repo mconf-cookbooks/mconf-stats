636fa1f3eb5358c0f67d51e9a808776668c12446
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2016-11-16 15:12:43 -0200

    Updating version to 5.0.1 for Filebeat and Packetbeat

--- attributes/packetbeat.rb
+++ attributes/packetbeat.rb
@@ -1,4 +1,4 @@
-default['mconf-stats']['beats']['packetbeat']['version']      = '5.0.0'
+default['mconf-stats']['beats']['packetbeat']['version']      = '5.0.1'
 default['mconf-stats']['beats']['packetbeat']['conf_dir']     = '/etc/packetbeat'
 default['mconf-stats']['beats']['packetbeat']['conf_file']    = ::File.join(node['mconf-stats']['beats']['packetbeat']['conf_dir'], 'packetbeat.yml')
 default['mconf-stats']['beats']['packetbeat']['service_name'] = 'packetbeat'

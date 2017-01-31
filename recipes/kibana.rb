7dfa9f8525ba16d4692d671acad984c574741a2b
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2017-01-31 11:56:13 -0200

    Separate 'populate_kibana' code from Kibana's recipe.

--- recipes/kibana.rb
+++ recipes/kibana.rb
@@ -89,34 +89,8 @@ template '/etc/systemd/system/kibana.service' do
   notifies :restart, 'service[kibana]', :delayed
 end
 
-# Accept data_bags to populate Kibana
-bag_name = node['mconf-stats']['kibana']['data_bag']
-begin
-  kibana_bag = Chef::DataBag.load(bag_name)
-  require 'json'
-rescue
-  kibana_bag = []
-  Chef::Log.warn("Could not find unencrypted data bag #{bag_name}")
-end
-
-# Populate Kibana index in Elasticsearch using Elasticdump
-kibana_bag.each_pair do |name, url|
-  items = Chef::DataBagItem.load(bag_name, name)
-  # so we get only the data we need, not all object
-  items = items.to_hash
-
-  input_file = ::File.join(Chef::Config[:file_cache_path], "kibana-seed-#{name}.json")
-  file input_file do
-    content items.to_json.to_s
-  end
-
-  bash "load kibana item #{name}" do
-    code Elasticdump.import_cmd(input_file,
-                                es_server,
-                                node['mconf-stats']['kibana']['es_index'])
-    action :run
-  end
-end
+# Populate Kibana from data_bags
+include_recipe "mconf-stats::populate_kibana"
 
 # In the newest versions of Kibana this file is created and used in the initialization
 # of the instance. But the last version of the cookbook don't treat this file very well.
